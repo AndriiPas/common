841a85a792275dbf2a185ca69bb6fdb8ed32db26
AndriiPas <andriypastushchuk@gmail.com>
2019-11-05 11:38:47 +0200

    update 05:11:2019 11:38

--- flask_practice/main.py
+++ flask_practice/main.py
@@ -13,37 +13,48 @@ def get_home_page():
 @app.route('/clock')
 def get_clock_page():
     arg = get_data("Alarm clock")
-    return render_template("Clock.html", title=arg['title'], text=arg['text'])
+    words = len(arg['text'].split())
+    return render_template("Clock.html", title=arg['title'], text=arg['text'], counts_of_word=words)
 
 
 @app.route('/Headphone')
 def get_headphones_page():
     arg = get_data("Headphones")
-    return render_template("Headphone.html", title=arg['title'], text=arg['text'])
+    words = len(arg['text'].split())
+    return render_template("Headphone.html", title=arg['title'], text=arg['text'], counts_of_word=words)
 
 
 @app.route('/ipod')
 def get_ipod_page():
     arg = get_data("iPod")
-    return render_template("ipod.html", title=arg['title'], text=arg['text'])
+    words = len(arg['text'].split())
+    return render_template("ipod.html", title=arg['title'], text=arg['text'], counts_of_word=words)
 
 
 @app.route('/calculator')
 def get_calculator_page():
     arg = get_data("Calculator")
-    return render_template("calculator.html", title=arg['title'], text=arg['text'])
+    words = len(arg['text'].split())
+    return render_template("calculator.html", title=arg['title'], text=arg['text'], counts_of_word=words)
 
 
 @app.route('/coffeemaker')
 def get_cofeemaker_page():
     arg = get_data("Coffeemaker")
-    return render_template("coffeemaker.html", title=arg['title'], text=arg['text'])
+    words = len(arg['text'].split())
+    return render_template("coffeemaker.html", title=arg['title'], text=arg['text'], counts_of_word=words)
 
 
 @app.route('/battery_charger')
 def get_battery_charger_page():
     arg = get_data("Battery charger")
-    return render_template("battery_charger.html", title=arg['title'], text=arg['text'])
+    words = len(arg['text'].split())
+    return render_template("battery_charger.html", title=arg['title'], text=arg['text'], counts_of_word=words)
+
+
+@app.route('/author')
+def get_author_page():
+    return render_template("author_.html")
 
 
 if __name__ == "__main__":
